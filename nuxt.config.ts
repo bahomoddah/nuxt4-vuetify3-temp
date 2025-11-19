aca8550c1bccf41cee12c5692a5077b613bd1690
bahomoddah <bahomoddah@gmail.com>
2025-11-19 08:48:47 +0300

    create nuxt 4

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["@nuxt/eslint", "@nuxt/image", "@nuxt/icon"],
+  devServer: {
+    port: 8080,
+    host: process.env.HOST || "localhost",
+  },
+});
