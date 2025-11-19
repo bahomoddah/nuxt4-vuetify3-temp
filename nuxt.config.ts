588e782ee8cdc3ec53eaa947d3e20ad78225a7ca
bahomoddah <bahomoddah@gmail.com>
2025-11-19 09:29:09 +0300

    add i18n 10

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,41 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/image", "@nuxt/icon"],
+
+  css: ["vuetify/lib/styles/main.sass"],
+
+  modules: ["@nuxt/eslint", "@nuxt/image", "@nuxt/icon", "@nuxtjs/i18n"],
+
+  build: {
+    transpile: ["vuetify"],
+  },
+
+  vite: {
+    ssr: {
+      noExternal: ["vuetify"],
+    },
+  },
+
+  i18n: {
+    langDir: "locales",
+    locales: [
+      {
+        code: "ar",
+        file: "ar.json",
+        name: "العربية",
+        dir: "rtl",
+      },
+      {
+        code: "en",
+        file: "en.json",
+        name: "English",
+        dir: "ltr",
+      },
+    ],
+    defaultLocale: "ar",
+    strategy: "prefix_except_default",
+  },
+
   devServer: {
     port: 8080,
     host: process.env.HOST || "localhost",
